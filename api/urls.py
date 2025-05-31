2f4b1b17899bc5012de9347e4d6fb15a113caf06
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-05-31 15:16:24 -0700

    finished viewsets and routes

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from videogame.views import VideogameViewSet
+from videogame.views import (
+    VideogameViewSet, VideogameImageViewSet, PostViewSet,PlatformViewSet,
+    GenreViewSet,TagViewSet,CategoryViewSet,PublisherViewSet,
+)
+
+
+
 
 router = DefaultRouter()
 router.register('videogames', VideogameViewSet)
+router.register('images', VideogameImageViewSet)
+router.register('posts', PostViewSet)
+router.register('platforms', PlatformViewSet)
+router.register('genres', GenreViewSet)
+router.register('tags', TagViewSet)
+router.register('categories', CategoryViewSet)
+router.register('publishers', PublisherViewSet)
 urlpatterns = router.urls
--- videogame/views.py
+++ videogame/views.py
@@ -8,7 +8,7 @@ from .models import (
     Tag, Category,
     VideogameImage, Post,
 )
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
 from .serializers import (
     VideogameImageSerializer,PlatformSerializer,
     GenreSerializer,TagSerializer,
@@ -24,13 +24,51 @@ class VideogameViewSet(ModelViewSet):
 
 
 class VideogameImageViewSet(ModelViewSet):
-    queryset = VideogameImage.objects.all()
+    queryset = VideogameImage.objects.select_related('videogame').all()
     serializer_class = VideogameImageSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_queryset(self):
         game_id = self.request.query_params.get('game_id')
         if game_id:
             return VideogameImage.objects.filter(game_id=game_id)
         return super().get_queryset()
+
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.select_related('game', 'author').all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class PlatformViewSet(ModelViewSet):
+    queryset = Platform.objects.all()
+    serializer_class = PlatformSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class TagViewSet(ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class PublisherViewSet(ModelViewSet):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
 
 
